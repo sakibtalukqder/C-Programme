615052bbb17df74989c8f3efa7c6302df416a149
sakibtalukqder <sakibtalukqder07@gmail.com>
2025-05-30 20:45:38 +0600

    Structured Programme init

---
+++ Lab-01/Programme/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf(
+        "The professor said: \"Today's topic is 'C Programming'.\" \n"
+        "Open the file at location: C:\\Users\\Student\\Documents\\Lab1"
+    );
+}
